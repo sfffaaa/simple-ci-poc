644882e7cf72ad4e14ffa03debcaac13e4e8db47
jaypan <pan@peaq.io>
2024-06-18 16:04:03 +0200

    Finish the setup/reset

---
+++ env.reset.bash
@@ -0,0 +1,9 @@
+WORK_DIRECTORY="/home/jaypan/Work/peaq/CI"
+
+deactive || echo "No need to deactivate venv"
+cd ${WORK_DIRECTORY}
+rm -rf ${WORK_DIRECTORY}/peaq-bc-test
+rm -rf ${WORK_DIRECTORY}/venv
+
+rm -rf ${WORK_DIRECTORY}/peaq-network-node
+rm -rf ${WORK_DIRECTORY}/parachain-launch
--- env.setup.bash
+++ env.setup.bash
@@ -1,5 +1,7 @@
 # Setup the projects
 
+WORK_DIRECTORY="/home/jaypan/Work/peaq/CI"
+
 # Check git install or not
 git --version > /dev/null 2>&1 || {
 	echo "Please install git"
@@ -16,10 +18,37 @@ docker compose version > /dev/null 2>&1 || {
 	exit 1
 }
 
-git clone git@github.com:peaqnetwork/peaq-network-node.git
+yarn --version > /dev/null 2>&1 || {
+	echo "Please install yarn"
+	exit 1
+}
+
+# # TODO Need to check
+# nvm --version > /dev/null 2>&1 || {
+# 	echo "Please install nvm"
+# 	exit 1
+# }
+
+# Setup the peaq-bc-test
+cd ${WORK_DIRECTORY}
 git clone git@github.com:peaqnetwork/peaq-bc-test.git
-(
-	git clone git@github.com:peaqnetwork/parachain-launch.git;
-	cd parachain-launch;
-	git checkout -b feat/simple-ci origin/feat/simple-ci
-)
+python3 -m venv ${WORK_DIRECTORY}/venv
+source ${WORK_DIRECTORY}/venv/bin/activate
+cd ${WORK_DIRECTORY}/peaq-bc-test
+pip3 install -r requirements.txt
+
+# Setup the peaq-network-node
+cd ${WORK_DIRECTORY}
+git clone git@github.com:peaqnetwork/peaq-network-node.git
+
+# Setup the parachain-launch
+cd ${WORK_DIRECTORY}
+git clone git@github.com:peaqnetwork/parachain-launch.git;
+cd ${WORK_DIRECTORY}/parachain-launch;
+git checkout -b feat/simple-ci origin/feat/simple-ci
+yarn build
+# error detective-postcss@5.1.1: The engine "node" is incompatible with this module. Expected version "12.x || 14.x || 16.x". Got "18.19.1"
+nvm install 16
+nvm use 16
+yarn install
+
